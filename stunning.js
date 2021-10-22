c9d2ac5dc5bebb112c88154b40c98e4b89b52389
jeyakarthick44 <jeyakarthick408@gmail.com>
2021-10-22 16:28:23 -0700

    final

---
+++ stunning.js
@@ -0,0 +1,74 @@
+console.log('Basic menu elements css changes')
+
+var menu_options = document.getElementById("item-1");
+
+menu_options.onmouseover = function() {
+    var item1 = document.getElementById('item-1')
+    item1.style.fontSize = "18px"
+    item1.style.transform = "translateX(1.2)"
+    item1.style.color = "black";
+    item1.style.textDecoration = "underline 2px"
+    item1.style.textDecorationColor = "#4733e0";
+}
+
+menu_options.onmouseout = function() {
+    var item1 = document.getElementById('item-1')
+    item1.style.fontSize = "16px"
+    item1.style.transform = "translateX(0)"
+    item1.style.color = "rgb(194, 188, 188)"
+    item1.style.textDecoration = "none";
+}
+
+var menu_options = document.getElementById("item-2");
+
+menu_options.onmouseover = function() {
+    var item1 = document.getElementById('item-2')
+    item1.style.fontSize = "18px"
+    item1.style.color = "black";
+    item1.style.textDecoration = "underline 2px"
+    item1.style.textDecorationColor = "#4733e0";
+}
+
+menu_options.onmouseout = function() {
+    var item1 = document.getElementById('item-2')
+    item1.style.fontSize = "16px"
+    item1.style.transform = "translateX(0)"
+    item1.style.color = "rgb(194, 188, 188)"
+    item1.style.textDecoration = "none";
+}
+
+var menu_options = document.getElementById("item-3");
+
+menu_options.onmouseover = function() {
+    var item1 = document.getElementById('item-3')
+    item1.style.fontSize = "18px"
+    item1.style.color = "black";
+    item1.style.textDecoration = "underline 2px"
+    item1.style.textDecorationColor = "#4733e0";
+}
+
+menu_options.onmouseout = function() {
+    var item1 = document.getElementById('item-3')
+    item1.style.fontSize = "16px"
+    item1.style.transform = "translateX(0)"
+    item1.style.color = "rgb(194, 188, 188)"
+    item1.style.textDecoration = "none";
+}
+
+var menu_options = document.getElementById("item-4");
+
+menu_options.onmouseover = function() {
+    var item1 = document.getElementById('item-4')
+    item1.style.fontSize = "18px"
+    item1.style.color = "black";
+    item1.style.textDecoration = "underline 2px"
+    item1.style.textDecorationColor = "#4733e0";
+}
+
+menu_options.onmouseout = function() {
+    var item1 = document.getElementById('item-4')
+    item1.style.fontSize = "16px"
+    item1.style.transform = "translateX(0)"
+    item1.style.color = "rgb(194, 188, 188)"
+    item1.style.textDecoration = "none";
+}
